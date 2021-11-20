9da69e31006c7f5d53bbd6e755122b765ccb2fec
Ann Sabie <annsabie1@gmail.com>
2021-11-19 21:37:20 -0600

    added homepage to packagejson

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-//import bootstrap from 'bootstrap'
-//import Section from "./components/Section";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer"
+import "./App.css"
 
 function App() {
   return (
-  
-  
     <div>
       <Navbar />
       <About />
