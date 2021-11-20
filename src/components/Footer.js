611a58abca3ac2a50d51032a1c014eabe69813ea
Ann Sabie <annsabie1@gmail.com>
2021-11-19 20:57:57 -0600

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+function Footer() {
+  return (
+    <footer id="footer">
+    <h4>
+      507.369.4700 annsabie1@gmail.com <a href="https://github.com/annsabie">Github</a>
+    </h4>
+  </footer>
+  );
+}
+
+export default Footer;
