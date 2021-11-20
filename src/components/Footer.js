21a22e3eeae83a35557d9fe728870ba85cb501ec
Ann Sabie <annsabie1@gmail.com>
2021-11-19 21:04:24 -0600

    added linkedin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,9 @@ function Footer() {
   return (
     <footer id="footer">
     <h4>
-      507.369.4700 annsabie1@gmail.com <a href="https://github.com/annsabie">Github</a>
+      507.369.4700
     </h4>
+    <h5><a href="mailto:annsabie1@gmail.com">Email</a>  <a href="https://github.com/annsabie">Github</a> <a href="https://www.linkedin.com/in/annsabie/">LinkedIn</a></h5>
   </footer>
   );
 }
