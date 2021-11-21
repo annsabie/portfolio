1b7eae677d59b94f44cade78280cb8da8ef00e91
Ann Sabie <annsabie1@gmail.com>
2021-11-20 20:49:54 -0600

    changed css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,20 +5,20 @@ function Navbar() {
   return (
     <nav id="nav" class="navbar navbar-expand-lg">
     <div class="container-fluid">
-      <a class="navbar-brand text-dark">Ann Sabie. Web Developer.</a>
+      <a class="navbar-brand" id="name">Ann Sabie. Web Developer.</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active text-dark" aria-current="page" href="./index.html">Home</a>
+            <a class="nav-link active" aria-current="page" href="./index.html">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-dark" href="#about">About</a>
+            <a class="nav-link" href="#about">About</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-dark" href="#footer">Contact</a>
+            <a class="nav-link" href="#footer">Contact</a>
           </li>
         </ul>
       </div>
